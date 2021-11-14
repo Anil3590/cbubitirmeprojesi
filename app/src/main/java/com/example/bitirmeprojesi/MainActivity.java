4953b872f6b79b6ee25c2fc419cc2ee73327538e
Anil3590 <ahmetanilsanli@gmail.com>
2021-11-15 01:53:44 +0300

    QR kod okuyucusu eklendi.

--- app/src/main/java/com/example/bitirmeprojesi/MainActivity.java
+++ app/src/main/java/com/example/bitirmeprojesi/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.bitirmeprojesi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    String[] permissions={
+            Manifest.permission.CAMERA
+
+    };
+    int PERM_CODE = 11;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,4 +35,19 @@ public class MainActivity extends AppCompatActivity {
         Intent scanner = new Intent(getApplicationContext(),ScannerActivity.class);
         startActivity(scanner);
     }
+
+    private boolean checkpermissions(){
+        List<String> listofpermissions = new ArrayList<>();
+        for (String perm: permissions){
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),perm)!= PackageManager.PERMISSION_GRANTED){
+                listofpermissions.add(perm);
+            }
+        }
+        if (!listofpermissions.isEmpty()){
+            ActivityCompat.requestPermissions(this, listofpermissions.toArray(new String[listofpermissions.size()]),PERM_CODE);
+            return false;
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/bitirmeprojesi/ScannerActivity.java
+++ app/src/main/java/com/example/bitirmeprojesi/ScannerActivity.java
@@ -1,14 +1,54 @@
 package com.example.bitirmeprojesi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 public class ScannerActivity extends AppCompatActivity {
+
+    TextView txt;
+    CodeScanner codeScanner;
+    CodeScannerView codescannerView;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+        txt = (TextView) findViewById(R.id.textView6);
+        codescannerView = (CodeScannerView) findViewById(R.id.scanner);
+        codeScanner = new CodeScanner(this,codescannerView);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txt.setText(result.getText());
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestCamera();
+        
+    }
+
+    private void requestCamera() {
+        codeScanner.startPreview();
     }
 }
