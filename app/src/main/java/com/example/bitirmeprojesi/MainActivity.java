cad0e58953ecfa9d1f0e16702722b0f19f9a97af
Anil3590 <ahmetanilsanli@gmail.com>
2022-01-08 00:07:03 +0300

    maps api key güncellendi

--- app/src/main/java/com/example/bitirmeprojesi/MainActivity.java
+++ app/src/main/java/com/example/bitirmeprojesi/MainActivity.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,34 @@ public class MainActivity extends AppCompatActivity {
 
     };
     int PERM_CODE = 11;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        checkpermissions();
     }
 
     public void gonder(View view) {
